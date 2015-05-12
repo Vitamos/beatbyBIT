3c44478897970142032600c7fe2826df45391dc3
Gomes <a38432@alunos.isel.pt>
2015-05-12 18:13:43 +0100

    v0.02

--- app/src/main/java/pt/isel/gomes/beatbybit/MainActivity.java
+++ app/src/main/java/pt/isel/gomes/beatbybit/MainActivity.java
@@ -1,7 +1,9 @@
 package pt.isel.gomes.beatbybit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -11,27 +13,20 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
- /*     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void launchRec(View view){
+        Intent intent = new Intent(this, Rec.class);
+        startActivity(intent);
     }
-
-  @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
+    public void launchCooper(View view){
+        Intent intent = new Intent(this, Cooper.class);
+        startActivity(intent);
+    }
+    public void launchAct(View view){
+        Intent intent = new Intent(this, Rec.class);
+        startActivity(intent);
+    }
+    public void launchSettings(View view){
+        Intent intent = new Intent(this, Rec.class);
+        startActivity(intent);
+    }
 }
