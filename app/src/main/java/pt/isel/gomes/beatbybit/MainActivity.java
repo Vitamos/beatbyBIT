dd0f7d2165e2cf1bba034edeb994e10a8423ad9a
Gomes <a38432@alunos.isel.pt>
2015-05-12 16:42:06 +0100

    v0.01

--- app/src/main/java/pt/isel/gomes/beatbybit/MainActivity.java
+++ app/src/main/java/pt/isel/gomes/beatbybit/MainActivity.java
@@ -1,12 +1,10 @@
 package pt.isel.gomes.beatbybit;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +13,14 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    @Override
+ /*     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
-    @Override
+  @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -35,5 +33,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
