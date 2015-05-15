00148892c2338259444d85c3c55a7248b8e10ca6
Gomes <a38432@alunos.isel.pt>
2015-05-15 18:18:52 +0100

    v0.14 - App key saves

--- app/src/main/java/pt/isel/gomes/beatbybit/MainActivity.java
+++ app/src/main/java/pt/isel/gomes/beatbybit/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 
@@ -17,6 +18,7 @@ import com.dropbox.client2.session.AppKeyPair;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 import pt.isel.gomes.beatbybit.util.Engine;
 
@@ -29,26 +31,11 @@ public class MainActivity extends Activity {
     final static private String APP_KEY = "un624qhagsgq8wb";
     final static private String APP_SECRET = "wid188gkonsbj62";
     private DropboxAPI<AndroidAuthSession> dropbox;
-    SharedPreferences sharedpreferences;
-    String accessToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         engine = new Engine();
-
-        sharedpreferences = getSharedPreferences("beat", Context.MODE_PRIVATE);
-        accessToken = sharedpreferences.getString("accessToken",null);
-        AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
-        AndroidAuthSession session;
-        if(accessToken != null){
-            session = new AndroidAuthSession(appKeys,accessToken);
-        }
-        else {
-            session = new AndroidAuthSession(appKeys);
-        }
-        dropbox = new DropboxAPI<AndroidAuthSession>(session);
-        dropbox.getSession().startOAuth2Authentication(MainActivity.this);
-
+        dropbox = getDropboxAPI();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (android.os.Build.VERSION.SDK_INT > 9) {
@@ -56,6 +43,28 @@ public class MainActivity extends Activity {
             StrictMode.setThreadPolicy(policy);
         }
     }
+
+    private DropboxAPI <AndroidAuthSession> getDropboxAPI(){
+        AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
+        AndroidAuthSession session = new AndroidAuthSession(appKeys);
+        DropboxAPI<AndroidAuthSession> mDBApi = new DropboxAPI<AndroidAuthSession>(session);
+        SharedPreferences sharedpreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String token = sharedpreferences.getString("token",null);
+        if (token == null){
+            mDBApi.getSession().startOAuth2Authentication(this);
+            return mDBApi;
+        }
+        mDBApi.getSession().setOAuth2AccessToken(token);
+        return mDBApi;
+    }
+
+    public void storePrefs(String token){
+        SharedPreferences sharedpreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString("token", token);
+        editor.commit();
+    }
+
     public void launchRec(View view){
         Intent intent = new Intent(this, Rec.class);
         intent.putExtra("engine",engine);
@@ -71,28 +80,41 @@ public class MainActivity extends Activity {
         intent.putExtra("engine",engine);
         startActivity(intent);
     }
+    private void writeToFile(String file,String data) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(file, Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
     public void launchSettings(View view) throws IOException, DropboxException {
         //Intent intent = new Intent(this, Settings.class);
        //intent.putExtra("engine",engine);
         //startActivity(intent);
         File file = new File(getFilesDir()+"myText.txt");
+
         file.createNewFile();
+        writeToFile("myText.txt","teste");
         FileInputStream inputStream = new FileInputStream(file);
         Entry response = dropbox.putFile("/magnum-opus.txt", inputStream,
                 file.length(), null, null);
         Log.i("DbExampleLog", "The uploaded file's rev is: " + response.rev);
+
     }
+
 
     protected void onResume() {
         super.onResume();
-        if (dropbox.getSession().authenticationSuccessful()) {
+        if (dropbox.getSession().authenticationSuccessful())  {
             try {
                 // Required to complete auth, sets the access token on the session
-                sharedpreferences = getSharedPreferences("beat", Context.MODE_PRIVATE);
                 dropbox.getSession().finishAuthentication();
-                accessToken = dropbox.getSession().getOAuth2AccessToken();
-                sharedpreferences.edit().putString("accessToken", accessToken);
-                sharedpreferences.edit().commit();
+                String token = dropbox.getSession().getOAuth2AccessToken();
+                storePrefs(token);
             } catch (IllegalStateException e) {
                 Log.i("DbAuthLog", "Error authenticating", e);
             }
