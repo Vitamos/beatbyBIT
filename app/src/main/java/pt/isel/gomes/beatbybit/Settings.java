b1dd65daf8583583b7e2b443badad4b452ad415f
Gomes <andre.fc.gomes@gmail.com>
2015-08-06 02:38:12 +0100

    v0.5

--- app/src/main/java/pt/isel/gomes/beatbybit/Settings.java
+++ app/src/main/java/pt/isel/gomes/beatbybit/Settings.java
@@ -1,8 +1,15 @@
 package pt.isel.gomes.beatbybit;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.util.Log;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import pt.isel.gomes.beatbybit.util.Engine;
 
@@ -15,6 +22,8 @@ public class Settings extends Activity {
         super.onCreate(savedInstanceState);
         Engine engine = (Engine) getIntent().getSerializableExtra("engine");
         getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefsFragment()).commit();
+
+
     }
 
     public static class PrefsFragment extends PreferenceFragment {
@@ -22,8 +31,26 @@ public class Settings extends Activity {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.preference);
+            EditTextPreference mac = (EditTextPreference) getPreferenceScreen().findPreference("mac_preference");
+            mac.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                String pattern = "^([0-9a-f]{2}[:-]){5}([0-9a-f]{2})$";
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    Boolean rtnval = true;
+                    String address = (String) newValue;
+                    Log.i("ADD ", address);
+                    if (!address.matches(pattern)){
+                        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setTitle("Invalid Input");
+                        builder.setMessage("Invalid MAC Address...");
+                        builder.setPositiveButton(android.R.string.ok, null);
+                        builder.show();
+                        rtnval = false;
+                    }
+                    return rtnval;
+                }
+            });
         }
     }
 }
